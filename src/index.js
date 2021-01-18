a3c0487ac925afa6d77d867f97a2a97296c22e74
Rafael Costa <rafael.costab@hotmail.com>
2021-01-17 22:52:45 -0300

    implementando viagem no tempo

--- src/index.js
+++ src/index.js
@@ -51,12 +51,13 @@ function Square (props) {
         history: [{
           squares: Array(9).fill(null),
         }],
-        xIsNext: true
+        stepNumber: 0,
+        xIsNext: true,
       };
     }
 
     handleClick(i) {
-      const history = this.state.history;
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
       const current = history[history.length -1];
       const squares = current.squares.slice();
 
@@ -68,13 +69,21 @@ function Square (props) {
         history: history.concat([{
           squares: squares,
         }]),
+        stepNumber: history.length,
         xIsNext: !this.state.xIsNext,
       });
     }
 
+    jumpTo(step){
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
+        });
+    }
+
     render() {
       const history = this.state.history;
-      const current = history[history.length -1];
+      const current = history[this.state.stepNumber];
       const winner = calculateWinner(current.squares);
       
       const moves = history.map((step, move) => {
@@ -82,10 +91,10 @@ function Square (props) {
           'Go to move #' + move : 
           'Go to game start';
         return (
-          <li>
+          <li key={move}>
             <button onClick={() => this.jumpTo(move)}>{desc}</button>
           </li>
-        ) 
+        );
       });
       
       let status;
